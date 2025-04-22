5f909b9aef863e4b0e6ddeedfe8eef24a25ad677
NguyenHoangDev77 <hoang.nguyen@globee.hk>
2025-04-22 16:34:58 +0700

    task 2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,8 +19,15 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+  let isbn = req.params?.isbn;
+  if(isbn){
+    let book = books[isbn];
+    if(book){
+      return res.status(200).json(book);
+    }
+  }
+  return res.status(404).json({message: "Not found!"});
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
